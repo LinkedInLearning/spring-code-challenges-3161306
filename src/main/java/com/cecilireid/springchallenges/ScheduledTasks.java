9fde0e6549ef910921800f3bd1411751a722415e
Cecili Reid <creid31@users.noreply.github.com>
2022-01-06 17:00:34 -0500

    Implement tasks

--- src/main/java/com/cecilireid/springchallenges/ScheduledTasks.java
+++ src/main/java/com/cecilireid/springchallenges/ScheduledTasks.java
@@ -2,13 +2,22 @@ package com.cecilireid.springchallenges;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class ScheduledTasks {
+    @Autowired
+    CateringJobRepository repository;
+
     private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
 
+    @Scheduled(cron = "*/10 * * * * *")
     public void reportOrderStats() {
-        
+        List<CateringJob> jobs = repository.findAll();
+        logger.info("We have {} jobs", jobs.size());
     }
 }
--- src/main/java/com/cecilireid/springchallenges/SpringChallengesApplication.java
+++ src/main/java/com/cecilireid/springchallenges/SpringChallengesApplication.java
@@ -2,8 +2,10 @@ package com.cecilireid.springchallenges;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringChallengesApplication {
 
 	public static void main(String[] args) {
