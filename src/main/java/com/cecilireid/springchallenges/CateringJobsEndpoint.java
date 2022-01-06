29b900101d054f49ae8a1af69b55fe9d02a963eb
Cecili Reid <creid31@users.noreply.github.com>
2022-01-06 17:01:45 -0500

    Implement Actuator

--- src/main/java/com/cecilireid/springchallenges/CateringJobsEndpoint.java
+++ src/main/java/com/cecilireid/springchallenges/CateringJobsEndpoint.java
@@ -1,12 +1,27 @@
 package com.cecilireid.springchallenges;
 
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
+
 @Component
+@Endpoint(id = "catering-jobs")
 public class CateringJobsEndpoint {
     private final CateringJobRepository cateringJobRepository;
 
     public CateringJobsEndpoint(CateringJobRepository cateringJobRepository) {
         this.cateringJobRepository = cateringJobRepository;
+    }
+
+    @ReadOperation
+    Map<Status, Long> getCateringJobsMetrics() {
+        return cateringJobRepository.findAll()
+                .stream()
+                .collect(groupingBy(CateringJob::getStatus, Collectors.counting()));
     }
 }
