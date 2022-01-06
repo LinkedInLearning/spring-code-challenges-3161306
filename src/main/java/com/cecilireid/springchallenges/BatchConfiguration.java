0733d475eedab9ec4cee07bb286382e79dbfb7d7
Cecili Reid <creid31@users.noreply.github.com>
2022-01-06 17:15:21 -0500

    Use Spring Batch

--- src/main/java/com/cecilireid/springchallenges/BatchConfiguration.java
+++ src/main/java/com/cecilireid/springchallenges/BatchConfiguration.java
@@ -9,43 +9,61 @@ import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfiguration {
-    @Autowired
-    public CateringJobRepository repository;
-
     @Autowired
     public JobBuilderFactory jobBuilderFactory;
 
     @Autowired
     public StepBuilderFactory stepBuilderFactory;
 
+    @Autowired
+    public CateringJobRepository repository;
+
     @Bean
     @StepScope
     public FlatFileItemReader<CateringJob> reader() {
         FlatFileItemReader<CateringJob> reader = new FlatFileItemReader<>();
+        reader.setResource(new ClassPathResource("upload.csv"));
+        reader.setLinesToSkip(1);
+
+        DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
+        tokenizer.setNames("id","customerName","phoneNumber","email","menu","noOfGuests","status");
+
         DefaultLineMapper<CateringJob> lineMapper = new DefaultLineMapper<>();
+        lineMapper.setLineTokenizer(tokenizer);
+        lineMapper.setFieldSetMapper(new CateringJobMapper());
+        reader.setLineMapper(lineMapper);
         return reader;
     }
 
     @Bean
     public RepositoryItemWriter<CateringJob> writer() {
-//        RepositoryItemWriter<CateringJob> writer = new RepositoryItemWriter<>();
-        return null;
-    }
-
-    @Bean
-    public Job uploadCateringJob() {
-        return null;
+        RepositoryItemWriter<CateringJob> writer = new RepositoryItemWriter<>();
+        writer.setRepository(repository);
+        return writer;
     }
 
     @Bean
     public Step step() {
-        return null;
+        return stepBuilderFactory.get("step")
+                .<CateringJob, CateringJob>chunk(10)
+                .reader(reader())
+                .writer(writer())
+                .build();
+    }
+
+    @Bean
+    public Job uploadCateringJob() {
+        return jobBuilderFactory.get("uploadCateringJob")
+                .start(step())
+                .build();
     }
 }
--- src/main/java/com/cecilireid/springchallenges/CateringJobMapper.java
+++ src/main/java/com/cecilireid/springchallenges/CateringJobMapper.java
@@ -4,7 +4,6 @@ import org.springframework.batch.item.file.mapping.FieldSetMapper;
 import org.springframework.batch.item.file.transform.FieldSet;
 import org.springframework.validation.BindException;
 
-
 public class CateringJobMapper implements FieldSetMapper<CateringJob> {
     @Override
     public CateringJob mapFieldSet(FieldSet fieldSet) throws BindException {
